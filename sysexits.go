f54b4bef53666468fb049eb28bf4127faa949ccd
tit <sblohin@yandex.ru>
2019-01-10 13:15:14 +0300

    write readme

--- sysexits.go
+++ sysexits.go
@@ -1,76 +1,81 @@
 package sysexits
 
 const (
-	// The command was used incorrectly, e.g., with the
-	// wrong number of arguments, a bad flag, a bad syntax
-	// in a parameter, or whatever.
-	ExUsage = 64
+  // Successful termination
+  ExOk = 0
 
-	// The input data was incorrect in some way.  This
-	// should only be used for user's data and not system
-	// files.
-	ExDataerr = 65
+  // The base value for sysexit codes
+  ExBase = 64
 
-	// An input file (not a system file) did not exist or
-	// was not readable.  This could also include errors
-	// like ``No message'' to a mailer (if it cared to
-	// catch it).
-	ExNoinput = 66
+  // The command was used incorrectly, e.g., with the
+  // wrong number of arguments, a bad flag, a bad syntax
+  // in a parameter, or whatever.
+  ExUsage = 64
 
-	// The user specified did not exist.  This might be
-	// used for mail addresses or remote logins.
-	ExNouser = 67
+  // The input data was incorrect in some way.  This
+  // should only be used for user's data and not system
+  // files.
+  ExDataerr = 65
 
-	// The host specified did not exist.  This is used in
-	// mail addresses or network requests.
+  // An input file (not a system file) did not exist or
+  // was not readable.  This could also include errors
+  // like ``No message'' to a mailer (if it cared to
+  // catch it).
+  ExNoinput = 66
 
-	ExNohost = 68
+  // The user specified did not exist.  This might be
+  // used for mail addresses or remote logins.
+  ExNouser = 67
 
-	// A service is unavailable.  This can occur if a support
-	// port program or file does not exist.  This can also
-	// be used as a catchall message when something you
-	// wanted to do doesn't work, but you don't know why.
-	ExUnavailable = 69
+  // The host specified did not exist.  This is used in
+  // mail addresses or network requests.
+  ExNohost = 68
 
-	// An internal software error has been detected.  This
-	// should be limited to non-operating system related
-	// errors as possible.
-	ExSoftware = 70
+  // A service is unavailable.  This can occur if a support
+  // port program or file does not exist.  This can also
+  // be used as a catchall message when something you
+  // wanted to do doesn't work, but you don't know why.
+  ExUnavailable = 69
 
-	// An operating system error has been detected.  This
-	// is intended to be used for such things as ``cannot
-	// fork'', ``cannot create pipe'', or the like.  It
-	// includes things like getuid returning a user that
-	// does not exist in the passwd file.
-	ExOserr = 71
+  // An internal software error has been detected.  This
+  // should be limited to non-operating system related
+  // errors as possible.
+  ExSoftware = 70
 
-	// Some system file (e.g., /etc/passwd, /var/run/utmp,
-	// etc.) does not exist, cannot be opened, or has some
-	// sort of error (e.g., syntax error).
-	ExOsfile = 72
+  // An operating system error has been detected.  This
+  // is intended to be used for such things as ``cannot
+  // fork'', ``cannot create pipe'', or the like.  It
+  // includes things like getuid returning a user that
+  // does not exist in the passwd file.
+  ExOserr = 71
 
-	// A (user specified) output file cannot be created.
-	ExCantcreat = 73
+  // Some system file (e.g., /etc/passwd, /var/run/utmp,
+  // etc.) does not exist, cannot be opened, or has some
+  // sort of error (e.g., syntax error).
+  ExOsfile = 72
 
-	// An error occurred while doing I/O on some file.
-	ExIoerr = 74
+  // A (user specified) output file cannot be created.
+  ExCantcreat = 73
 
-	// Temporary failure, indicating something that is not
-	// really an error.  In sendmail, this means that a
-	// mailer (e.g.) could not create a connection, and
-	// the request should be reattempted later.
-	ExTempfail = 75
+  // An error occurred while doing I/O on some file.
+  ExIoerr = 74
 
-	// The remote system returned something that was ``not
-	// possible'' during a protocol exchange.
-	ExProtocol = 76
+  // Temporary failure, indicating something that is not
+  // really an error.  In sendmail, this means that a
+  // mailer (e.g.) could not create a connection, and
+  // the request should be reattempted later.
+  ExTempfail = 75
 
-	// You did not have sufficient permission to perform
-	// the operation.  This is not intended for file sys-tem system
-	// tem problems, which should use EX_NOINPUT or
-	// EX_CANTCREAT, but rather for higher level. permissions.
-	ExNoperm = 77
+  // The remote system returned something that was ``not
+  // possible'' during a protocol exchange.
+  ExProtocol = 76
 
-	// Something was found in an unconfigured or misconfigured state.
-	ExConfig = 78
+  // You did not have sufficient permission to perform
+  // the operation.  This is not intended for file sys-tem system
+  // tem problems, which should use EX_NOINPUT or
+  // EX_CANTCREAT, but rather for higher level. permissions.
+  ExNoperm = 77
+
+  // Something was found in an unconfigured or misconfigured state.
+  ExConfig = 78
 )
